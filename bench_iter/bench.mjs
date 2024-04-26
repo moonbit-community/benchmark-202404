305c6d97e562a05eddad376cf4aa3d6d0064c836
文宇祥 <hackwaly@qq.com>
2024-04-26 23:41:08 +0800

    add js iter

--- bench_iter/bench.mjs
+++ bench_iter/bench.mjs
@@ -1,6 +1,7 @@
 import Bench from "tinybench";
 import * as MoonbitBenchIter from './bench_iter_moonbit.mjs';
 import * as PlainJsBenchIter from './bench_iter_js.mjs';
+import * as PlainJs2BenchIter from './bench_iter_js2.mjs';
 import * as KotlinJsBenchIter from './bench_iter_kotlin_js.cjs';
 import data from './data/data.json' with { type: "json" };
 
@@ -11,6 +12,10 @@ bench.add('Moonbit', () => {
 
 bench.add('Plain Js', () => {
     PlainJsBenchIter.run(data);
+});
+
+bench.add('Js Iterator Helpers', () => {
+    PlainJs2BenchIter.run(data);
 });
 
 bench.add('Kotlin Js', () => {
